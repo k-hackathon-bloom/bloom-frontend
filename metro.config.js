f13063b7474d0f87cdf50f55c2861e6ee913535f
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-10-09 22:47:40 +0900

    config: SVG 파일을 컴포넌트로 사용할 수 있도록 설정
    - ref: #1

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { assetExts, sourceExts } = getDefaultConfig(__dirname).resolver;
 
 /**
  * Metro configuration
@@ -6,6 +7,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
