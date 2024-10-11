707372b1689dbf90fd0a57930ca3fd2a5261565a
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-10-11 19:43:20 +0900

    fix: 현재 스크린의 높이를 기준으로도 반응형 디자인을 적용할 수 있도록 responsive 함수 수정
    - ref: #9

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -1,9 +1,22 @@
 import { Dimensions } from 'react-native';
 
-const baseDesignScreenSize = 393;
-const { width } = Dimensions.get('window');
+const BASE_DESIGN_SCREEN_WIDTH = 393;
+const BASE_DESIGN_SCREEN_HEIGHT = 852;
 
-export default function responsive(baseDesignElementSize: number): number {
-  const screenRatio = width / baseDesignScreenSize;
+const { width, height } = Dimensions.get('window');
+
+const calculateScreenRatio = (baseSize: number, screenSize: number): number => {
+  return baseSize / screenSize;
+};
+
+export default function responsive(
+  baseDesignElementSize: number,
+  basis?: 'width' | 'height',
+): number {
+  const screenRatio =
+    basis === 'height'
+      ? calculateScreenRatio(height, BASE_DESIGN_SCREEN_HEIGHT)
+      : calculateScreenRatio(width, BASE_DESIGN_SCREEN_WIDTH);
+
   return baseDesignElementSize * screenRatio;
 }
