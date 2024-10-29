2cd94d7268ad03ccfb3f907899a49ab79305c80b
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-10-29 12:06:04 +0900

    feature: 퀘스트 목록 조회 API 연동
    - ref: #45

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import ScreenLayout from '@screens/ScreenLayout';
 import { userDataAtom } from '@recoil/atoms';
+import Toast from 'react-native-toast-message';
 import HomeHeader from '@screens/Home/components/HomeHeader';
 import DailyProgress from '@screens/Home/components/DailyProgress';
+import apiClient from '@apis/client';
+import Quest from '@type/Quest';
 
 const Home = () => {
   const userData = useRecoilValue(userDataAtom);
+  const [allQuests, setAllQuests] = useState<Quest[]>([]);
+
+  const fetchAllQuests = useCallback(async () => {
+    try {
+      const response = await apiClient.get('/api/quests/available');
+      const questsFromServer = response.data.quests.map((quest: Quest) => ({
+        id: quest.id,
+        iconUrl: quest.iconUrl,
+        title: quest.title,
+        maxCount: quest.maxCount,
+      }));
+      setAllQuests(questsFromServer);
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: '퀘스트 데이터를 가져오는 데 실패했습니다.',
+        text2: String(error),
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchAllQuests();
+  }, [fetchAllQuests]);
 
   return (
     <ScreenLayout>
