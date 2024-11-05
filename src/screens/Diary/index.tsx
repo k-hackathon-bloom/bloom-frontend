a60d531a152d52e68f04e74ed849d73effd6e8f2
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-11-06 02:29:06 +0900

    feature: 다이어리 스크린 헤더 구현
    - ref: #51

--- src/screens/Diary/index.tsx
+++ src/screens/Diary/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import StyledText from '@components/common/StyledText';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
 import ScreenLayout from '@screens/ScreenLayout';
+import DiaryHeader from '@screens/Diary/components/DiaryHeader';
+import getFormatedDate from '@utils/getFormatedDate';
 
 const Diary = () => {
+  const [date, setDate] = useState(new Date());
   return (
     <ScreenLayout>
-      <StyledText>Diary Screen</StyledText>
+      <DiaryHeader title={getFormatedDate(date)} setDate={setDate} />
+      <ScrollView showsVerticalScrollIndicator={false} />
     </ScreenLayout>
   );
 };
