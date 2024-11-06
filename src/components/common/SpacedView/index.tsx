2e6395cecd112a920cb6daa3e07a0690ce7f90b9
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-11-06 09:05:54 +0900

    style: 오늘의 질문, 오늘의 명언 컴포넌트 생성 및 다이어리 스크린에 적용
    - ref: #53

--- src/components/common/SpacedView/index.tsx
+++ src/components/common/SpacedView/index.tsx
@@ -3,17 +3,27 @@ import { View } from 'react-native';
 import styled from 'styled-components/native';
 import responsive from '@utils/responsive';
 
-const GappedView = styled(View)<{ gap: number }>`
+const GappedView = styled(View)<{ gap: number; direction?: 'row' | 'column' }>`
   gap: ${(props) => responsive(props.gap, 'height')}px;
+  flex-direction: ${(props) => props.direction || 'column'};
 `;
 
 interface SpacedViewProps {
   gap: number;
+  direction?: 'row' | 'column';
   children: React.ReactNode;
 }
 
-const SpacedView: React.FC<SpacedViewProps> = ({ gap, children }) => {
-  return <GappedView gap={gap}>{children}</GappedView>;
+const SpacedView: React.FC<SpacedViewProps> = ({
+  gap,
+  children,
+  direction,
+}) => {
+  return (
+    <GappedView gap={gap} direction={direction}>
+      {children}
+    </GappedView>
+  );
 };
 
 export default SpacedView;
--- src/screens/Diary/index.tsx
+++ src/screens/Diary/index.tsx
@@ -3,13 +3,19 @@ import { ScrollView } from 'react-native';
 import ScreenLayout from '@screens/ScreenLayout';
 import DiaryHeader from '@screens/Diary/components/DiaryHeader';
 import getFormatedDate from '@utils/getFormatedDate';
+import DailyInspiration from '@screens/Diary/components/DailyInspiration';
 
 const Diary = () => {
   const [date, setDate] = useState(new Date());
+  const [question, setQuestion] = useState('');
+  const [saying, setSaying] = useState('');
+
   return (
     <ScreenLayout>
       <DiaryHeader title={getFormatedDate(date)} setDate={setDate} />
-      <ScrollView showsVerticalScrollIndicator={false} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <DailyInspiration question={question} saying={saying} />
+      </ScrollView>
     </ScreenLayout>
   );
 };
