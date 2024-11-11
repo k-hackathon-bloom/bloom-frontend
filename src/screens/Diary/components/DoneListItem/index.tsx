314fbac60a6b7654303934b8a8519ac0b1ff5104
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-11-11 10:50:39 +0900

    style: 개별 항목 삭제를 위하여 DoneListItem 컴포넌트를 스와이프할 수 있도록 수정
    - ref: #57

--- src/screens/Diary/components/DoneListItem/index.tsx
+++ src/screens/Diary/components/DoneListItem/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { TouchableOpacity, Animated } from 'react-native';
 import styled from 'styled-components/native';
+import { SwipeRow } from 'react-native-swipe-list-view';
 import ItemLayout from '@components/ItemLayout';
 import StyledText from '@components/common/StyledText';
 import responsive from '@utils/responsive';
+import useAnimatedValue from '@hooks/useAnimatedValue';
+import DeleteIcon from '@assets/icons/delete.svg';
 
 const DoneListContainer = styled(ItemLayout)`
   flex-direction: row;
   align-items: center;
   justify-content: flex-start;
   height: ${responsive(60, 'height')}px;
-  border-radius: ${responsive(8, 'height')}px;
   padding: 0 ${responsive(20, 'height')}px;
+  border-radius: ${responsive(8, 'height')}px;
+  overflow: hidden;
 `;
 
 const AddTaskContainer = styled(DoneListContainer)`
@@ -25,8 +29,59 @@ const ButtonWrapper = styled(TouchableOpacity)`
 
 const DoneListTitle = styled(StyledText)`
   font-size: ${responsive(14, 'height')}px;
-  letter-spacing: ${responsive(-0.5)}px;
+  letter-spacing: ${responsive(-0.5, 'height')}px;
 `;
+
+const DeleteTaskButton = styled(TouchableOpacity)`
+  height: 100%;
+  background-color: ${(props) => props.theme.COLORS.BUTTON_RED};
+  align-items: center;
+  justify-content: center;
+`;
+
+const HiddenItemContainer = styled(DoneListContainer)`
+  padding: 0;
+`;
+
+interface HiddenItemsProps {
+  taskId: number;
+  onDeleteTask: (taskId: number) => void;
+  swipeValue: Animated.Value;
+}
+
+const HiddenItems: React.FC<HiddenItemsProps> = ({
+  taskId,
+  onDeleteTask,
+  swipeValue,
+}) => {
+  const { animatedValue: buttonWidth, animateToValue } = useAnimatedValue(0);
+
+  useEffect(() => {
+    const targetWidth = responsive(55, 'height');
+    const listenerId = swipeValue.addListener(({ value }) => {
+      const width = Math.min(value, targetWidth);
+      animateToValue(width, 0, 0, false).start();
+    });
+
+    return () => {
+      swipeValue.removeListener(listenerId);
+    };
+  }, [swipeValue, animateToValue]);
+
+  return (
+    <HiddenItemContainer>
+      <Animated.View style={{ width: buttonWidth }}>
+        <DeleteTaskButton onPress={() => onDeleteTask(taskId)}>
+          <DeleteIcon
+            style={{ position: 'absolute', left: responsive(20, 'height') }}
+            width={responsive(15, 'height')}
+            height={responsive(15, 'height')}
+          />
+        </DeleteTaskButton>
+      </Animated.View>
+    </HiddenItemContainer>
+  );
+};
 
 export const AddTaskButton = ({ onPress }: { onPress: () => void }) => {
   return (
@@ -38,13 +93,47 @@ export const AddTaskButton = ({ onPress }: { onPress: () => void }) => {
   );
 };
 
-const DoneListItem = ({ title }: { title: string }) => {
+interface DoneListItemProps {
+  id: number;
+  title: string;
+  handleOpenModal: () => void;
+  handleDeleteTask: (taskId: number) => void;
+  setIsSwiping: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const DoneListItem: React.FC<DoneListItemProps> = ({
+  id,
+  title,
+  handleOpenModal,
+  handleDeleteTask,
+  setIsSwiping,
+}) => {
+  const swipeRowRef = useRef<SwipeRow<unknown> | null>(null);
+  const { animatedValue: swipeValue } = useAnimatedValue(0);
+
   return (
-    <DoneListContainer>
-      <ButtonWrapper>
-        <DoneListTitle>{title}</DoneListTitle>
-      </ButtonWrapper>
-    </DoneListContainer>
+    // @ts-expect-error
+    <SwipeRow
+      ref={swipeRowRef}
+      leftOpenValue={responsive(55, 'height')}
+      disableLeftSwipe={true}
+      onSwipeValueChange={(value) => {
+        swipeValue.setValue(value.value);
+      }}
+      swipeGestureBegan={() => setIsSwiping(true)}
+      swipeGestureEnded={() => setIsSwiping(false)}
+    >
+      <HiddenItems
+        taskId={id}
+        onDeleteTask={(taskId) => handleDeleteTask(taskId)}
+        swipeValue={swipeValue}
+      />
+      <DoneListContainer>
+        <ButtonWrapper onPress={handleOpenModal}>
+          <DoneListTitle numberOfLines={1}>{title}</DoneListTitle>
+        </ButtonWrapper>
+      </DoneListContainer>
+    </SwipeRow>
   );
 };
 
