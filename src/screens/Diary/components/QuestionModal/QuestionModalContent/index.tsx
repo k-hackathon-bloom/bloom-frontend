6325dc4c8ab959e4caa206dfca7e40ed6d5429d4
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-12-14 23:46:13 +0900

    feature: 오늘의 질문 답변 등록 및 수정 API 연동
    - ref: #63

--- src/screens/Diary/components/QuestionModal/QuestionModalContent/index.tsx
+++ src/screens/Diary/components/QuestionModal/QuestionModalContent/index.tsx
@@ -5,6 +5,8 @@ import StyledButton from '@components/common/StyledButton';
 import WordBreakText from '@components/common/WordBreakText';
 import SpacedView from '@components/common/SpacedView';
 import responsive from '@utils/responsive';
+import apiClient from '@apis/client';
+import Toast from 'react-native-toast-message';
 
 const ContentContainer = styled(View)`
   width: 100%;
@@ -53,6 +55,18 @@ const QuestionModalContent: React.FC<QuestionModalContentProps> = ({
   setAnswer,
   editable,
 }) => {
+  const handleSaveAnswer = async () => {
+    try {
+      await apiClient.put('/api/daily-question/answer', { answer });
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: '변경 사항을 저장하는 데 실패했습니다.',
+        text2: String(error),
+      });
+    }
+  };
+
   return (
     <ContentContainer>
       <SpacedView gap={responsive(20, 'height')}>
@@ -66,7 +80,7 @@ const QuestionModalContent: React.FC<QuestionModalContentProps> = ({
           editable={editable}
         />
         <ButtonContainer>
-          <StyledButton title="저장" onPress={() => {}} />
+          <StyledButton title="저장" onPress={() => handleSaveAnswer()} />
         </ButtonContainer>
       </SpacedView>
     </ContentContainer>
