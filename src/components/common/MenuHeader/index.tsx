30d9f76c256e1c244513e601d15e5098ee891fdc
itwillbeoptimal <itwillbeoptimal@gmail.com>
2024-12-17 07:07:21 +0900

    style: 메뉴 헤더가 최상위 항목일 때 margin-top 0으로 설정
    - ref: #64

--- src/components/common/MenuHeader/index.tsx
+++ src/components/common/MenuHeader/index.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { View } from 'react-native';
 import styled from 'styled-components/native';
 import StyledText from '@components/common/StyledText';
 import responsive from '@utils/responsive';
+
+const MenuWrapper = styled(View)<{ isTopLevel?: boolean }>`
+  margin-bottom: ${responsive(25, 'height')}px;
+  margin-top: ${(props) => (props.isTopLevel ? 0 : responsive(25, 'height'))}px;
+`;
 
 const MenuTitle = styled(StyledText)`
   font-family: ${(props) => props.theme.FONT_WEIGHTS.BOLD};
   font-size: ${responsive(16, 'height')}px;
   align-self: flex-start;
-  margin: ${responsive(25, 'height')}px 0;
 `;
 
 interface MenuHeaderProps {
   title: string;
+  isTopLevel?: boolean;
 }
 
-const MenuHeader: React.FC<MenuHeaderProps> = ({ title }) => {
-  return <MenuTitle>{title}</MenuTitle>;
+const MenuHeader: React.FC<MenuHeaderProps> = ({ title, isTopLevel }) => {
+  return (
+    <MenuWrapper isTopLevel={isTopLevel}>
+      <MenuTitle>{title}</MenuTitle>
+    </MenuWrapper>
+  );
 };
 
 export default MenuHeader;
