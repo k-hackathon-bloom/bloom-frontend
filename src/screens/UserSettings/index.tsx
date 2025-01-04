4c37a9e980aac4df352bcdfa132bc419d70f7569
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-01-05 04:42:37 +0900

    feature: 로그아웃 기능 구현
    - ref: #69

--- src/screens/UserSettings/index.tsx
+++ src/screens/UserSettings/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import ScreenLayout from '@screens/ScreenLayout';
 import UserSettingsHeader from '@screens/UserSettings/components/UserSettingsHeader';
+import SettingsItem from '@screens/UserSettings/components/SettingsItem';
+import useNavigate from '@hooks/useNavigate';
+import { ScrollView } from 'react-native';
 
 const UserSettings = () => {
+  const { navigateTo } = useNavigate();
+
+  const logout = async () => {
+    await AsyncStorage.multiRemove(['accessToken', 'refreshToken']);
+    navigateTo('Login');
+  };
+
   return (
     <ScreenLayout>
       <UserSettingsHeader title="설정" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <SettingsItem title="로그아웃" onPress={logout} isLast={true} />
+      </ScrollView>
     </ScreenLayout>
   );
 };
