19c847c15311aedc4f69f5661f6ba470af40b160
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-05-13 02:06:05 +0900

    feature: 유리병 메시지 타입 정의
    - ref: #75

---
+++ src/types/Message.ts
@@ -0,0 +1,16 @@
+interface BaseMessage {
+  messageId: number;
+  title: string;
+  postCardUrl: string;
+  negativity?: number;
+  timestamp: string;
+}
+
+export interface Message extends BaseMessage {}
+
+export interface MessageDetails {
+  message: BaseMessage & {
+    content: string;
+  };
+  isReacted: boolean;
+}
